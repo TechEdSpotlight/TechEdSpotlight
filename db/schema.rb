75c5e4c078ba83e312fe926da2896e66b70fa01d
Joanna Johnson <jjohnson@iqt.org>
2016-04-30 16:52:34 -0400

    Update registration form to capture new user fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430202145) do
+ActiveRecord::Schema.define(version: 20160430202330) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "title",      null: false
@@ -27,6 +27,26 @@ ActiveRecord::Schema.define(version: 20160430202145) do
   add_index "stories", ["published"], name: "index_stories_on_published"
   add_index "stories", ["title"], name: "index_stories_on_title", unique: true
   add_index "stories", ["user_id"], name: "index_stories_on_user_id"
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

350fa37290ab3746caa36701f33c6e8a026a2617
Peter Lai <peter.yd.lai@gmail.com>
2016-07-09 14:27:35 -0400

    static homepage

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+      #TODO pass in most recent stories and most popular stories
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   resources :stories
   # The priority is based upon order of creation: first created -> highest priority.

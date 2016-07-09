5e8d155c537fe02c5e6561ddeb8218e8b39dd83c
Peter Lai <peter.yd.lai@gmail.com>
2016-07-09 15:24:04 -0400

    add dynamic most recent stories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-      #TODO pass in most recent stories and most popular stories
+      @most_recent_stories = Story.last(3)
+      @most_popular_stories = Story.first(3) #TODO calculate actual most popular stories
   end
 end

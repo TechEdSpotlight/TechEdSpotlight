1e99e7b241d124f46c178da03d4a2661d185170e
Pamela Vong <pamelavong@gmail.com>
2016-07-19 01:06:43 +0000

    add aws sdk and s3 config for production confirguation of paperclip

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'devise'
 gem 'acts-as-taggable-on', '~> 3.4'
 
 gem "paperclip", "~> 5.0.0"
+gem "aws-sdk"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,27 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  
+  # Amazon S3 settings for Paperclip uploads
+  config.paperclip_defaults = {
+
+    storage: :s3,
+
+    s3_protocol: 'http',
+
+    s3_credentials: {
+
+      bucket: ENV.fetch("AWS_BUCKET"),
+
+      access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
+
+      secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+
+      s3_region: ENV.fetch("AWS_REGION"),
+
+    }
+
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
